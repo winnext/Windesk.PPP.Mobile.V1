bcac1452008331248fa0c35ab0cf894034744b07
muratcanyusufoglu <muratcan.ysfgl@gmail.com>
2023-07-24 11:22:40 +0300

    Flutter file changes

--- lib/feature/constants/paths/service_tools.dart
+++ lib/feature/constants/paths/service_tools.dart
@@ -4,23 +4,23 @@ import 'package:flutter/material.dart';
 class ServiceTools {
   const ServiceTools._();
 
-  // static const String baseUrlV1 =
-  //     'https://demo.signumtte.com/windesk/app/webroot/integration/WindeskMobile.php?use_rest=1&wsusername=wdmobile&wspassword=wdsgnm1017_&token=';
-  // static const String baseUrlV2 = 'http://windeskmobile.signumtte.com';
-  // static const tokenV1 = 'wddemo!_';
-  // static const tokenV2 = 'demo!';
-  // static const attachPath = "https://demo.signumtte.com/windesk";
-  // static const attachPathLive =
-  //     "http://10.32.12.42/windesktest/xreadattach.php?token=$tokenV1&id=";
-  // static const facilityName = "İstanbul Finans Merkezi";
-
   static const String baseUrlV1 =
-      'http://geskwdtestapp.gaziantep.yerel/windesk/app/webroot/integration/WindeskMobile.php?use_rest=1&wsusername=wdmobile&wspassword=wdsgnm1017_&token=';
-  static const String baseUrlV2 = 'http://wd-mobile-test.gaziantep.yerel';
-  static const tokenV1 = 'anteptest!_';
-  static const tokenV2 = 'anteptest!';
-  static const attachPath = "http://geskwdtestapp.gaziantep.yerel/windesk";
+      'https://demo.signumtte.com/windesk/app/webroot/integration/WindeskMobile.php?use_rest=1&wsusername=wdmobile&wspassword=wdsgnm1017_&token=';
+  static const String baseUrlV2 = 'http://windeskmobile.signumtte.com';
+  static const tokenV1 = 'wddemo!_';
+  static const tokenV2 = 'demo!';
+  static const attachPath = "https://demo.signumtte.com/windesk";
   static const attachPathLive =
-      "http://geskwdtestapp.gaziantep.yerel/windesk/xreadattach.php?token=$tokenV1&id=";
-  static const facilityName = "Gaziantep Şehir Hastanesi";
+      "http://10.32.12.42/windesktest/xreadattach.php?token=$tokenV1&id=";
+  static const facilityName = "İstanbul Finans Merkezi";
+
+  // static const String baseUrlV1 =
+  //     'http://geskwdtestapp.gaziantep.yerel/windesk/app/webroot/integration/WindeskMobile.php?use_rest=1&wsusername=wdmobile&wspassword=wdsgnm1017_&token=';
+  // static const String baseUrlV2 = 'http://wd-mobile-test.gaziantep.yerel';
+  // static const tokenV1 = 'anteptest!_';
+  // static const tokenV2 = 'anteptest!';
+  // static const attachPath = "http://geskwdtestapp.gaziantep.yerel/windesk";
+  // static const attachPathLive =
+  //     "http://geskwdtestapp.gaziantep.yerel/windesk/xreadattach.php?token=$tokenV1&id=";
+  // static const facilityName = "Gaziantep Şehir Hastanesi";
 }
