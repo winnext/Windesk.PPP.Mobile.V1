0f58d4e127ddda177b342210976d783e9b81d2ae
muratcanyusufoglu <muratcan.ysfgl@gmail.com>
2023-08-09 09:31:36 +0300

    issue changes

--- lib/feature/constants/paths/service_tools.dart
+++ lib/feature/constants/paths/service_tools.dart
@@ -4,29 +4,27 @@ import 'package:flutter/material.dart';
 class ServiceTools {
   const ServiceTools._();
 
-  static const String baseUrlV1 =
-      'https://demo.signumtte.com/windesk/app/webroot/integration/WindeskMobile.php?use_rest=1&wsusername=wdmobile&wspassword=wdsgnm1017_&token=';
-  static const String baseUrlV2 = 'http://windeskmobile.signumtte.com';
-  static const tokenV1 = 'wddemo!_';
-  static const tokenV2 = 'demo!';
-  static const attachPath = "https://demo.signumtte.com/windesk";
-  static const attachPathLive =
-      "http://10.32.12.42/windesktest/xreadattach.php?token=$tokenV1&id=";
-  static const facilityName = "İstanbul Finans Merkezi";
+  // static const String baseUrlV1 =
+  //     'https://demo.signumtte.com/windesk/app/webroot/integration/WindeskMobile.php?use_rest=1&wsusername=wdmobile&wspassword=wdsgnm1017_&token=';
+  // static const String baseUrlV2 = 'http://windeskmobile.signumtte.com';
+  // static const tokenV1 = 'wddemo!_';
+  // static const tokenV2 = 'demo!';
+  // static const attachPath = "https://demo.signumtte.com/windesk";
+  // static const attachPathLive =
+  //     "http://10.32.12.42/windesktest/xreadattach.php?token=$tokenV1&id=";
+  // static const facilityName = "İstanbul Finans Merkezi";
 
   //// ESH PREPROD
-// const PPP_PROJECT = "esh";
-// const BASE_URL_V2 = 'http://windeskmobiletest.eskisehir.yerel';
-// const TOKEN_V1 = 'wdeshtest!_';
-// const TOKEN_V2 = 'eshtest!';
-// const ATTACHPATHLIVE =
-//     "http://10.126.12.21/esh/xreadattach.php?token=${TOKEN_V1}&id=";
-// const base_url_v1 =
-//     'http://10.126.12.21/esh/app/webroot/integration/WindeskMobile.php?use_rest=1&wsusername=' +
-//         'wdmobile' +
-//         '&wspassword=' +
-//         'wdsgnm1017_' +
-//         '&token=';
+  static const PPP_PROJECT = "esh";
+  static const BASE_URL_V2 = 'http://windeskmobiletest.eskisehir.yerel';
+  static const TOKEN_V1 = 'wdeshtest!_';
+  static const TOKEN_V2 = 'eshtest!';
+  static const ATTACHPATHLIVE = "http://10.126.12.21/esh/xreadattach.php?token=${TOKEN_V1}&id=";
+  static const base_url_v1 = 'http://10.126.12.21/esh/app/webroot/integration/WindeskMobile.php?use_rest=1&wsusername=' +
+      'wdmobile' +
+      '&wspassword=' +
+      'wdsgnm1017_' +
+      '&token=';
 
   // static const String baseUrlV1 =
   //     'http://geskwdtestapp.gaziantep.yerel/windesk/app/webroot/integration/WindeskMobile.php?use_rest=1&wsusername=wdmobile&wspassword=wdsgnm1017_&token=';
