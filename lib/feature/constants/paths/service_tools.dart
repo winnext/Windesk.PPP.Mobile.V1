5de8eb2596f3569e6ccec1a048db5dfc4cd50a4c
MertSaygili <mert71719601@gmail.com>
2023-08-11 11:45:57 +0300

    service pathes updated

--- lib/feature/constants/paths/service_tools.dart
+++ lib/feature/constants/paths/service_tools.dart
@@ -25,16 +25,14 @@ class ServiceTools {
   // static const facilityName = "İstanbul Finans Merkezi";
 
   //// ESH PREPROD
-  static const PPP_PROJECT = "esh";
-  static const BASE_URL_V2 = 'http://windeskmobiletest.eskisehir.yerel';
-  static const TOKEN_V1 = 'wdeshtest!_';
-  static const TOKEN_V2 = 'eshtest!';
-  static const ATTACHPATHLIVE = "http://10.126.12.21/esh/xreadattach.php?token=${TOKEN_V1}&id=";
-  static const base_url_v1 = 'http://10.126.12.21/esh/app/webroot/integration/WindeskMobile.php?use_rest=1&wsusername=' +
-      'wdmobile' +
-      '&wspassword=' +
-      'wdsgnm1017_' +
-      '&token=';
+  static const pppProject = "esh";
+  static const baseUrlV2 = 'http://windeskmobiletest.eskisehir.yerel';
+  static const tokenV1 = 'wdeshtest!_';
+  static const tokenV2 = 'eshtest!';
+  static const attachPathLive = "http://10.126.12.21/esh/xreadattach.php?token=$tokenV1&id=";
+  static const baseUrlV1 =
+      'http://10.126.12.21/esh/app/webroot/integration/WindeskMobile.php?use_rest=1&wsusername=wdmobile&wspassword=wdsgnm1017_&token=';
+  static const facilityName = 'Eskişehir Şehir Hastanesi';
 
   // static const String baseUrlV1 =
   //     'http://geskwdtestapp.gaziantep.yerel/windesk/app/webroot/integration/WindeskMobile.php?use_rest=1&wsusername=wdmobile&wspassword=wdsgnm1017_&token=';
