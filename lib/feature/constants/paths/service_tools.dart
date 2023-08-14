b9179a5136a340a77f3e585e2d17412bb6814dfc
muratcanyusufoglu <muratcan.ysfgl@gmail.com>
2023-08-14 09:24:14 +0300

    changes

--- lib/feature/constants/paths/service_tools.dart
+++ lib/feature/constants/paths/service_tools.dart
@@ -7,7 +7,7 @@ class ServiceTools {
   // static const String baseUrlV1 =
   //     'https://demo.signumtte.com/windesk/app/webroot/integration/WindeskMobile.php?use_rest=1&wsusername=wdmobile&wspassword=wdsgnm1017_&token=';
   // static const String baseUrlV2 = 'http://windeskmobile.signumtte.com';
-  // static const tokenV1 = 'wddemo!_';
+  // static const tokenV1 = 'wddemo!_';2
   // static const tokenV2 = 'demo!';
   // static const attachPath = "https://demo.signumtte.com/windesk";
   // static const attachPathLive = "http://10.32.12.42/windesktest/xreadattach.php?token=$tokenV1&id=";
