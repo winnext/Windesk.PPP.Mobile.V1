68aba57ec40cfe2b2faf86afbf4b775cc3a2815b
MertSaygili <mert71719601@gmail.com>
2023-08-14 12:30:10 +0300

    detail buttons added

--- lib/product/screens/work_order/work_order_detail/view/work_order_detail_screen.dart
+++ lib/product/screens/work_order/work_order_detail/view/work_order_detail_screen.dart
@@ -1,13 +1,15 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:wm_ppp_4/feature/components/cards/custom_work_order_detail_card.dart';
-import 'package:wm_ppp_4/feature/components/loading/custom_loading_indicator.dart';
-import 'package:wm_ppp_4/feature/constants/style/custom_paddings.dart';
-import 'package:wm_ppp_4/product/screens/work_order/work_order_detail/provider/work_order_detail_provider_main.dart';
 
 import '../../../../../feature/components/appbar/custom_main_appbar.dart';
+import '../../../../../feature/components/cards/custom_work_order_detail_card.dart';
+import '../../../../../feature/components/loading/custom_loading_indicator.dart';
+import '../../../../../feature/constants/other/app_icons.dart';
 import '../../../../../feature/constants/other/colors.dart';
+import '../../../../../feature/constants/style/border_radius.dart';
+import '../../../../../feature/constants/style/custom_paddings.dart';
+import '../provider/work_order_detail_provider_main.dart';
 
 @RoutePage()
 class WorkOrderDetailScreen extends StatelessWidget {
@@ -38,6 +40,8 @@ class WorkOrderDetailScreen extends StatelessWidget {
       child: Column(
         children: [
           CustomWorkOrderDetailCard(data: value.workOrderDetailsModel),
+          const SizedBox(height: 16),
+          _WorkDetailButtons(value: value),
         ],
       ),
     );
@@ -55,6 +59,50 @@ class WorkOrderDetailScreen extends StatelessWidget {
       elevation: 4,
       popFunction: () {},
       actions: const [],
+    );
+  }
+}
+
+class _WorkDetailButtons extends StatelessWidget {
+  const _WorkDetailButtons({required this.value});
+
+  final WorkOrderDetailMainProvider value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 50,
+          width: 50,
+          decoration: BoxDecoration(borderRadius: CustomBorderRadius.circularBorderRadius, color: APPColors.Main.black),
+          child: Icon(AppIcons.camera, color: APPColors.Main.white),
+        ),
+        const SizedBox(height: 12),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _elevatedButton(APPColors.Login.blue, 'Başlat', false),
+            const SizedBox(width: 15),
+            _elevatedButton(APPColors.Main.red, 'Bitir', true),
+          ],
+        )
+      ],
+    );
+  }
+
+  Widget _elevatedButton(Color color, String text, bool isActive) {
+    return SizedBox(
+      width: 100,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: color,
+          shape: RoundedRectangleBorder(borderRadius: CustomBorderRadius.mediumBorderRadius),
+          disabledBackgroundColor: APPColors.Main.black,
+        ),
+        onPressed: isActive ? () {} : null,
+        child: Text(text, style: TextStyle(color: APPColors.Main.white, fontSize: 14, fontWeight: FontWeight.w400)),
+      ),
     );
   }
 }
