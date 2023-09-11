3a115f72ce8be7b84c8aceb5f8f9348e02db6ca0
sgnm-ozkanaydin <ozkan.aydin@signumtte.com>
2023-09-11 09:17:07 +0300

    update esh endpoint

--- lib/feature/constants/paths/service_tools.dart
+++ lib/feature/constants/paths/service_tools.dart
@@ -15,16 +15,16 @@ class ServiceTools {
   // static const facilityName = "İstanbul Finans Merkezi";
 //ESH PREPROD
   static const String baseUrlV1 =
-      'http://10.126.12.21/esh/app/webroot/integration/WindeskMobile.php?use_rest=1&wsusername=wdmobile&wspassword=wdsgnm1017_&token=';
+      'http://10.126.12.21/esh3/app/webroot/integration/WindeskMobile.php?use_rest=1&wsusername=wdmobile&wspassword=wdsgnm1017_&token=';
   static const String baseUrlV2 = 'http://windeskmobiletest.eskisehir.yerel';
   static const tokenV1 = 'wdeshtest!_';
   static const tokenV2 = 'eshtest!';
-  static const attachPath = "http://10.126.12.21/esh";
+  static const attachPath = "http://10.126.12.21/esh3";
   static const attachPathLive =
-      "http://10.126.12.21/esh/xreadattach.php?token=$tokenV1&id=";
+      "http://10.126.12.21/esh3/xreadattach.php?token=$tokenV1&id=";
   static const facilityName = "Eskişehir Şehir Hastanesi";
   static const appName = "ESH";
-  
+
   // GSH PREPROD
   // static const pppProject = "antep";
   // static const baseUrlV2 = 'http://wd-mobile-test.gaziantep.yerel';
