ee09d479827d360d8834c10a80af55143a8383f5
MertSaygili <mert71719601@gmail.com>
2023-10-16 13:57:56 +0300

    merge

--- lib/feature/service/global_services.dart/auth_service/auth_service_repository_impl.dart
+++ lib/feature/service/global_services.dart/auth_service/auth_service_repository_impl.dart
@@ -16,21 +16,10 @@ import 'auth_service_repository.dart';
 
 class AuthServiceRepositoryImpl extends AuthServiceRepository {
   @override
-<<<<<<< HEAD
   Future<Either<LoginModel, CustomServiceException>> login(String username, String password) async {
     final String deviceId = await SharedManager().getString(SharedEnum.deviceId);
     final String deviceType = await SharedManager().getString(SharedEnum.deviceType);
     final String firebaseToken = await SharedManager().getString(SharedEnum.firebaseToken);
-=======
-  Future<Either<LoginModel, CustomServiceException>> login(
-      String username, String password) async {
-    final String deviceId =
-        await SharedManager().getString(SharedEnum.deviceId);
-    final String deviceType =
-        await SharedManager().getString(SharedEnum.deviceType);
-    final String firebaseToken =
-        await SharedManager().getString(SharedEnum.firebaseToken);
->>>>>>> e30a4ae2f780fbfbe94d6cb4293c34b2b198e1a4
     @override
     String url =
         '${ServiceTools.baseUrlV1}${ServiceTools.tokenV1}$deviceId&action=loginCheck&username=$username&password=$password&platform=$deviceType&version=3&mobileV2=true';
@@ -55,18 +44,15 @@ class AuthServiceRepositoryImpl extends AuthServiceRepository {
       return Left(loginModel);
     } catch (error) {
       super.logger.e(error.toString());
-      return Right(CustomServiceException(
-          message: CustomServiceMessages.loginError, statusCode: '400'));
+      return Right(CustomServiceException(message: CustomServiceMessages.loginError, statusCode: '400'));
     }
   }
 
   @override
   Future<Either<bool, CustomServiceException>> logout(String username) async {
-    final String deviceId =
-        await SharedManager().getString(SharedEnum.deviceId);
+    final String deviceId = await SharedManager().getString(SharedEnum.deviceId);
 
-    String url =
-        '${ServiceTools.baseUrlV1}${ServiceTools.tokenV1}$deviceId&action=logout&username=$username';
+    String url = '${ServiceTools.baseUrlV1}${ServiceTools.tokenV1}$deviceId&action=logout&username=$username';
 
     try {
       final response = await super.dio.get(
@@ -79,19 +65,16 @@ class AuthServiceRepositoryImpl extends AuthServiceRepository {
         super.logger.i('logout success');
         return const Left(true);
       } else {
-        return Right(CustomServiceException(
-            message: CustomServiceMessages.logoutError, statusCode: '400'));
+        return Right(CustomServiceException(message: CustomServiceMessages.logoutError, statusCode: '400'));
       }
     } catch (error) {
       super.logger.e(error.toString());
-      return Right(CustomServiceException(
-          message: CustomServiceMessages.logoutError, statusCode: '400'));
+      return Right(CustomServiceException(message: CustomServiceMessages.logoutError, statusCode: '400'));
     }
   }
 
   @override
-  Future<Either<CheckAccesTokenModel, CustomServiceException>> checkAccessToken(
-      String token) async {
+  Future<Either<CheckAccesTokenModel, CustomServiceException>> checkAccessToken(String token) async {
     String url = 'http://localhost:3012/user/checkAccessToken';
 
     try {
@@ -105,19 +88,15 @@ class AuthServiceRepositoryImpl extends AuthServiceRepository {
       if (response.statusCode == 200 || response.statusCode == 201) {
         final data = response.data;
 
-        CheckAccesTokenModel checkAccesTokenModel =
-            CheckAccesTokenModel.fromJson(data);
+        CheckAccesTokenModel checkAccesTokenModel = CheckAccesTokenModel.fromJson(data);
 
         return Left(checkAccesTokenModel);
       } else {
-        return Right(CustomServiceException(
-            message: CustomServiceMessages.tokenChangeError,
-            statusCode: '400'));
+        return Right(CustomServiceException(message: CustomServiceMessages.tokenChangeError, statusCode: '400'));
       }
     } catch (error) {
       super.logger.e(error.toString());
-      return Right(CustomServiceException(
-          message: CustomServiceMessages.tokenChangeError, statusCode: '400'));
+      return Right(CustomServiceException(message: CustomServiceMessages.tokenChangeError, statusCode: '400'));
     }
   }
 }
